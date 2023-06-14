8b7983fbb55817b18d01d9a95d465ddc880fdefa
Takla Zidan <ztakla3@gmail.com>
2023-06-14 21:55:33 +0300

    my first commit

---
+++ UNIVERSITY_ADMISSION/UNIVERSITY_ADMISSION/Student/serializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+
+from .models import Student,Certificate,CertificationMarks
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = (
+            'first_name',
+            'middle_name',
+            'last_name',
+            )
+
+# class CertificateSerializer(serializers.ModelSerializer):
+#     Total_Mark = serializers.IntegerField(
+#         source = 'Certificate_Id.Total_Marks',
+#     )
+#     student_details = StudentSerializer(source = 'Student_Id')
+#     class Meta:
+#         model = Certificate
+#         fields = (
+#             'Total_Mark',
+#             )
+
+
+class CertificationMarksSerializer(serializers.ModelSerializer):
+    mark_name = serializers.CharField(
+        source = 'mark_id.mark',
+    )
+    class Meta:
+        model = CertificationMarks
+        fields = (
+            'mark_name',
+            'score',
+            )
+
+class StudentCertificationMarksSerializer(serializers.ModelSerializer):
+    student = StudentSerializer(
+        source = 'student_id',
+        read_only = True,
+    )
+    marks = serializers.SerializerMethodField(
+        read_only = True,
+    )
+    class Meta:
+        model = Certificate
+        fields = (
+            'student',
+            'marks',
+            'total_marks',
+            )
+
+    def get_marks(self, obj):
+        serializer = CertificationMarksSerializer(
+            obj.marks_certificate_set,
+            many= True,
+        )
+        return serializer.data
+    
