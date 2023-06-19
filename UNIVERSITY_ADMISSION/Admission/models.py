108724b8e630597ec0dae83e86531553c27b1a4a
Takla3 <ztakla3@gmail.com>
2023-06-19 18:55:56 +0300

    Second commit

---
+++ UNIVERSITY_ADMISSION/Admission/models.py
@@ -0,0 +1,116 @@
+
+from django.db import models
+from enum import Enum, unique
+from django.core.validators import MinValueValidator, MaxValueValidator
+# Create your models here.
+
+
+@unique
+class Gender(Enum):
+    ذكر = True
+    أنثى = False
+
+    def __str__(self):
+        return 'ذكر' if self.value else 'أنثى'
+
+
+GENDER_CHOICES = [
+    (True, 'ذكر'),
+    (False, 'أنثى'),
+]
+
+
+class Governorate(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+
+
+class CertificateType(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=40)
+
+
+class Marks(models.Model):
+    id = models.AutoField(primary_key=True)
+    mark = models.CharField(max_length=40)
+    certificate_type_id = models.ForeignKey(
+        CertificateType,
+        on_delete=models.CASCADE,
+    )
+
+
+class AdmissionType(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=30)
+
+
+class Majors(models.Model):
+    id = models.IntegerField(primary_key=True)
+    certificate_type_id = models.ForeignKey(
+        CertificateType,
+        on_delete=models.CASCADE,
+    )
+    governorate_id = models.ForeignKey(
+        Governorate,
+        on_delete=models.CASCADE,
+
+    )
+    admission_type_id = models.ForeignKey(
+        AdmissionType,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+    year = models.IntegerField(
+        validators=[MinValueValidator(2020), MaxValueValidator(9999)]
+    )
+    gender = models.BooleanField(
+        [(gender.value, str(gender)) for gender in Gender],
+        # choices=GENDER_CHOICES,
+        blank=True
+    )
+    min_pass_grade = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+
+class MajorMark(models.Model):
+    mark_id = models.ForeignKey(
+        Marks,
+        on_delete=models.CASCADE,
+    )
+    major_id = models.ForeignKey(
+        Majors,
+        on_delete=models.CASCADE,
+        related_name='mark_major_set',
+    )
+    min_pass_grade = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+
+
+class consideredMarks(models.Model):
+    mark_id = models.ForeignKey(
+        Marks,
+        on_delete=models.CASCADE,
+    )
+    major_id = models.ForeignKey(
+        Majors,
+        on_delete=models.CASCADE,
+        related_name='considered_marks_major_set',
+    )
+    min_pass_grade = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+
+
+class Language(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+
+class Status(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
---
+++ UNIVERSITY_ADMISSION/Student/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from Student.serializers import StudentCertificationMarksSerializer
+from Student.models import Certificate
+from django_filters.rest_framework import DjangoFilterBackend
+# Create your views here.
+class StudentCertificationMarksViewSet(ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = StudentCertificationMarksSerializer
+    lookup_field= 'seat_number'
