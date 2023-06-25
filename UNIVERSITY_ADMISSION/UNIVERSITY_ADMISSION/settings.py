2791f422e3dc52bbc4b513cdcbc8f373320e5948
Takla3 <ztakla3@gmail.com>
2023-06-25 15:09:46 +0300

    default db sqlite3

--- UNIVERSITY_ADMISSION/UNIVERSITY_ADMISSION/settings.py
+++ UNIVERSITY_ADMISSION/UNIVERSITY_ADMISSION/settings.py
@@ -96,7 +96,12 @@ WSGI_APPLICATION = 'UNIVERSITY_ADMISSION.wsgi.application'
 #     }
 # }
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 # #     'default': {
 # #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
